9308c494250d497453e59931161c6371420fc54d
jidegithub <olutolajide@gmail.com>
2020-05-01 07:33:11 +0100

    deploying to heroku:first-run

--- client/src/API.js
+++ client/src/API.js
@@ -1,12 +1,12 @@
 const axios = require('axios');
-const API_URL = 'http://localhost:1337';
+// const API_URL = 'http://localhost:1337';
 
 export async function listLogEntries(){
-    const response = await axios.get(`${API_URL}/api/logs`);
+    const response = await axios.get(`/api/logs`);
     return response;
 }
 
 export async function createLogEntries(entry) {
-    const response = await axios.post(`${API_URL}/api/logs`, entry);
+    const response = await axios.post(`/api/logs`, entry);
     return response;
 }
--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('path')
 const morgan = require('morgan');
 const helmet = require('helmet');
 const cors = require('cors');
@@ -12,11 +13,11 @@ const logs = require('./api/logs')
 
 const app = express();
 
-mongoose.connect(process.env.DATABASE_URL, {
-    useUnifiedTopology: true,
-    useNewUrlParser: true,
-    useFindAndModify: false 
-});
+// mongoose.connect(process.env.DATABASE_URL, {
+//     useUnifiedTopology: true,
+//     useNewUrlParser: true,
+//     useFindAndModify: false 
+// });
 
 // callback format
 // MongoClient.connect(connectionString, {
@@ -31,11 +32,17 @@ mongoose.connect(process.env.DATABASE_URL, {
 // const db = process.env.MONGO_DB_ATLAS_URL;
 
 // MongoClient.connect(db, 
-//  { useUnifiedTopology: true, useFindAndModify: false })
+//  { useUnifiedTopology: true})
 // .then(client => {
 //     console.log('Connected to Database')
 //     const db = client.db('travel_log')
 // })
+const db = process.env.MONGODB_URL;
+mongoose.connect(db, 
+{ useNewUrlParser: true, useUnifiedTopology: true},
+() => {console.log('Connected to Database')}
+)
+    
 
 app.use(morgan('common'));
 app.use(helmet());
@@ -43,16 +50,18 @@ app.use(cors({
     origin: process.env.CORS_ORIGIN
 }));
 
+
+
 //bodyparsing middleware to interprete the body of the request
 // there are other bodyparsers
 // since  i am only going to be dealing with json, i se the below
 app.use(express.json());
 
-app.get('/', (req, res) => {
-    res.json({
-        message: 'hello world!'
-    });
-});
+// app.get('/', (req, res) => {
+//     res.json({
+//         message: 'hello world!'
+//     });
+// });
 
 // other routes
 app.use('/api/logs', logs);
@@ -63,6 +72,13 @@ app.use(middlewares.notFound);
 app.use(middlewares.errorHandler);
 
 const port = process.env.PORT || 1337
+
+if(process.env.NODE_ENV === 'production'){
+    app.use(express.static(path.join(__dirname, 'client/build')))
+    app.get('*', (req, res) => {
+        res.sendFile(path.join(__dirname + '/client/build/index/html'))
+    })
+}
 
 app.listen(port, () => {
     console.log(`listening at http://localhost:${port}`)
